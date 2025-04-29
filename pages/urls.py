ebb18f6e4e45583830515b38549b835742f13b4d
Thee5176 <theerapong5176@gmail.com>
2025-04-29 18:50:14 +0900

    View Function

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import homepage_view
 
+urlpatterns = [
+    path("", homepage_view, name="homepage"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# def homepage_view(request):
+#     message = "Welcome, this is the homepage of my website."
+#     return message
+
+# def homepage_view(request):
+#     title = "Homepage"
+#     message = "Welcome, this is the homepage of my website."
+#     return HttpResponse(message)
+
+
+def homepage_view(request):
+    title = "Homepage"
+    message = "Welcome, this is the homepage of my website."
+
+    context = {
+        "title": title,
+        "message": message,
+    }
+
+    return render(request, "home.html", context)
